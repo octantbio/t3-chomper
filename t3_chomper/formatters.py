a4232dc3c1a05f3d8c370fb48d2a6e7ce3e9ac0f
John C. Faver <jfaver@posteo.net>
2025-09-16 09:04:20 -0400

    add column checks

--- t3_chomper/formatters.py
+++ t3_chomper/formatters.py
@@ -20,24 +20,31 @@ def generate_registration_pka_file(
         pka_pkas_col: name of the column in the pKa data file with pKa data
 
     Returns:
-        Merged dataframe with columns from the registartion_csv and the estimated pKa data from the pKa data file
+        Merged dataframe with columns from the registration_csv and the estimated pKa data from the pKa data file
     """
 
     regi_df = pd.read_csv(registration_csv)
-    pka_df = pd.read_csv(pka_csv, usecols=[pka_id_col, pka_pkas_col]).rename(
+    if registration_id_col not in regi_df.columns:
+        raise ValueError(
+            f"Expected column {registration_id_col} is missing in {registration_csv}"
+        )
+
+    pka_df = pd.read_csv(pka_csv)
+    for col in [pka_id_col, pka_pkas_col]:
+        if col not in pka_df.columns:
+            raise ValueError(f"Expected column {col} is missing in {pka_csv}")
+
+    pka_df = pka_df[[pka_id_col, pka_pkas_col]].rename(
         columns={pka_id_col: registration_id_col}
     )
 
-    merged_df = pd.merge(
-        regi_df,
-        pka_df,
-        how="left",
-        on=registration_id_col,
-    )
+    merged_df = pd.merge(regi_df, pka_df, how="left", on=registration_id_col)
 
-    missing_rows = merged_df["reformatted_pkas"].isna().sum()
-    if missing_rows:
-        missing_row_ids = merged_df[merged_df["reformatted_pkas"].isna()]["ID"]
-        raise ValueError(f"Some rows have missing data: {missing_row_ids}")
+    missing_row_count = merged_df[pka_pkas_col].isna().sum()
+    if missing_row_count:
+        missing_row_ids = merged_df[merged_df[pka_pkas_col].isna()][registration_id_col]
+        raise ValueError(
+            f"{missing_row_count} rows have missing data: {missing_row_ids}"
+        )
 
     return merged_df
