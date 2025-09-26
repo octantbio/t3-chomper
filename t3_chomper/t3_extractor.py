c9028cd8c2e0def9b79deb6f79e0c22208c92339
John C. Faver <jfaver@posteo.net>
2025-09-26 11:13:28 -0400

    command group for extraction

--- t3_chomper/t3_extractor.py
+++ t3_chomper/t3_extractor.py
@@ -7,71 +7,62 @@ from typing import Union
 import click
 import pandas as pd
 
-from t3_chomper.extractors import UVMetricPKaT3RExtractor
+from t3_chomper.extractors import (
+    UVMetricPKaT3RExtractor,
+    LogPT3RExtractor,
+    AssayCategory,
+)
 from t3_chomper.logger import get_logger
 
 logger = get_logger(__name__)
 
 
-def parse_t3_pka_xml(file: Union[str, Path]) -> tuple:
+class FileOrPathExtractor:
     """
-    Read XML file from the Sirus T3 machine and extract pKa data.
-
-    Returns:
-        Tuple of (pka, stdev, ionic_strength, temp)
+    Class to extract data from one or more t3r result files.
     """
-    extractor = UVMetricPKaT3RExtractor(file)
 
-    pka = extractor.mean_pka_values
-    stdev = extractor.mean_pka_std_values
-    ionic_strength = extractor.mean_pka_ionic_strengths
-    temp = extractor.mean_pka_temperatures
+    def __init__(
+        self,
+        t3_dir: Union[str, None] = None,
+        t3_file: Union[str, None] = None,
+    ):
+        if (t3_dir is None and t3_file is None) or (t3_dir and t3_file):
+            raise ValueError("Provide either t3_dir or t3_file, but not both.")
 
-    return pka, stdev, ionic_strength, temp
+        if t3_dir:
+            self._t3_files = glob.glob(f"{t3_dir}/*.t3r")
+            if not self._t3_files:
+                raise FileNotFoundError(f"No .t3r files found in directory: {t3_dir}")
+        else:
+            self._t3_files = [t3_file]
 
+    def _parse_files(self, assay_category: AssayCategory) -> pd.DataFrame:
+        """ """
+        rows = []
+        for file in self._t3_files:
+            logger.debug(f"Parsing T3R XML file: {file}")
+            if assay_category == AssayCategory.PKA:
+                pka_parser = UVMetricPKaT3RExtractor(file)
+                results = pka_parser.result_dict
+            elif assay_category == AssayCategory.LOGP:
+                logp_parser = LogPT3RExtractor(file)
+                results = logp_parser.result_dict
+            else:
+                raise ValueError(f"Unknown assay category: {assay_category}")
+            results["file"] = Path(file).name
+            rows.append(results)
+        df = pd.DataFrame(rows)
+        return df
 
-def parse_pkas(
-    t3_dir: Union[str, None] = None, t3_file: Union[str, None] = None
-) -> pd.DataFrame:
-    """
-    Parse one or multiple .t3 files from directory or single file into a DataFrame.
+    def parse_pka_files(self):
+        return self._parse_files(AssayCategory.PKA)
 
-    Only one of t3_dir or t3_file must be provided.
-    """
-    if (t3_dir is None and t3_file is None) or (t3_dir and t3_file):
-        raise ValueError("Provide either t3_dir or t3_file, but not both.")
-
-    if t3_dir:
-        t3_files = glob.glob(f"{t3_dir}/*.t3r")
-        if not t3_files:
-            raise FileNotFoundError(f"No .t3r files found in directory: {t3_dir}")
-    else:
-        t3_files = [t3_file]
-
-    rows = []
-    for file in t3_files:
-        logger.debug(f"Parsing T3 XML file: {file}")
-        try:
-            pka, stdev, ionic_strength, temp = parse_t3_pka_xml(file)
-        except Exception as e:
-            logger.error(f"Error parsing file {file}: {e}")
-            continue
-
-        rows.append(
-            {
-                "file": Path(file).name,
-                "pka": pka,
-                "stdev": stdev,
-                "ionic_strength": ionic_strength,
-                "temperature": temp,
-            }
-        )
-
-    df = pd.DataFrame(rows)
-    return df
+    def parse_logp_files(self):
+        return self._parse_files(AssayCategory.LOGP)
 
 
-@click.command()
+@click.group()
 @click.option(
     "--t3-dir",
     type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True),
@@ -89,15 +80,33 @@ def parse_pkas(
     type=click.Path(exists=False, file_okay=True),
     default="pka_results.csv",
 )
+def t3r_extract():
+    click.echo(f"Extracting data from t3r files.")
+
+
+@t3r_extract.command()
 def pka(t3_dir, t3_file, output_csv):
     """
-    CLI entry point for parsing T3 pKa XML files and exporting to CSV.
+    Parse T3R pKa XML files and export to CSV.
     """
     logger.info("Running T3 pKa parsing")
-    df = parse_pkas(t3_dir, t3_file)
+    extractor = FileOrPathExtractor(t3_dir=t3_dir, t3_file=t3_file)
+    df = extractor.parse_pka_files()
+    logger.info(f"Finished parsing, writing to {output_csv}")
+    df.to_csv(output_csv, index=False)
+
+
+@t3r_extract.command()
+def logp(t3_dir, t3_file, output_csv):
+    """
+    Parse T3R logp XML files and export to CSV.
+    """
+    logger.info("Running T3 logp parsing")
+    extractor = FileOrPathExtractor(t3_dir=t3_dir, t3_file=t3_file)
+    df = extractor.parse_logp_files()
     logger.info(f"Finished parsing, writing to {output_csv}")
     df.to_csv(output_csv, index=False)
 
 
 if __name__ == "__main__":
-    pka()
+    pass
